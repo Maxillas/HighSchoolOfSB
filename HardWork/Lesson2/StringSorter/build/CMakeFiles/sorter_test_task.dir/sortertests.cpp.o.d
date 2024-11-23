28c2f949143588504facaf87394028dce3602ee3
Maxillas <maksanchutin@mail.ru>
2024-11-23 11:21:03 +0500

    ref

--- HardWork/Lesson2/StringSorter/build/CMakeFiles/sorter_test_task.dir/sortertests.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/sorter_test_task.dir/sortertests.cpp.o: \
-  /usr/lib/llvm-18/lib/clang/18/share/asan_ignorelist.txt \
-  /home/maxillas/Lessons/HighSchoolOfSB/HardWork/Lesson2/StringSorter/sortertests.cpp
