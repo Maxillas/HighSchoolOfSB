a07721f640dbf75fe884f17354644c11cb80ce9f
Maxillas <maksanchutin@mail.ru>
2024-11-23 11:21:52 +0500

    ref

--- HardWork/Lesson2/StringSorter/build/Debug/qtcsettings.cmake
+++
@@ -1,5 +0,0 @@
-# This file is managed by Qt Creator, do not edit!
-
-set("CMAKE_GENERATOR" "Ninja" CACHE "STRING" "" FORCE)
-set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
-set("CMAKE_PROJECT_INCLUDE_BEFORE" "/home/maxillas/Lessons/HighSchoolOfSB/HardWork/Lesson2/StringSorter/build/Debug/.qtc/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
