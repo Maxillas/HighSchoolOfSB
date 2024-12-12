edd50290f77cd0a25e73daca40fb44a89e21b816
Max Anchutin <100233370+Maxillas@users.noreply.github.com>
2024-12-12 12:58:17 +0500

    Update test_insert.py

--- Algorithms/Sorting/test_insert.py
+++ Algorithms/Sorting/test_insert.py
@@ -1,16 +1,6 @@
 import unittest
 import insert
 
-def InsertionSortStep(array, step, i):
-        
-    for index in range (i + step, len(array), step):
-        for inserIndex in range (i, index, step):
-            if(array[index] < array[inserIndex]):
-                array[index], array[inserIndex] = array[inserIndex], array[index]
-
-
-
-
 class TestBST(unittest.TestCase):
     
     def test_InsertSort(self):
