4aa3cd6d5e464635a14d47ae6e523db6cdd379f4
Max Anchutin <100233370+Maxillas@users.noreply.github.com>
2024-12-12 12:57:57 +0500

    add tests shell test_insert.py

--- Algorithms/Sorting/test_insert.py
+++ Algorithms/Sorting/test_insert.py
@@ -31,7 +31,29 @@ class TestBST(unittest.TestCase):
         self.assertEqual(test2, [])
         InsertionSortStep(test1, 3, 0)
         self.assertEqual(test2, [])
+    def test_ShellSort(self):
+        test1 = [7,6,5,4,3,2,1]
 
+        
+        self.assertEqual(SequenceElem(1), 4)
+        self.assertEqual(SequenceElem(2), 13)
+        self.assertEqual(SequenceElem(3), 40)
+        self.assertEqual(SequenceElem(4), 121)
+        self.assertEqual(SequenceElem(5), 364)
+
+        self.assertEqual(KnuthSequence(15), [13,4,1])
+        self.assertEqual(KnuthSequence(13), [13,4,1])
+        self.assertEqual(KnuthSequence(12), [4,1])
+        self.assertEqual(KnuthSequence(4), [4,1])
+        self.assertEqual(KnuthSequence(3), [4,1])
+        self.assertEqual(KnuthSequence(350), [364,13,4,1])
+        self.assertEqual(KnuthSequence(367), [364,4,1])
+        self.assertEqual(KnuthSequence(0), [])
+        self.assertEqual(KnuthSequence(1), [1])
+                
+        test5 = [5,6,7,1,4,2,3]
+        ShellSorting(test5)
+        self.assertEqual(test5, [1,2,3,4,5,6,7])
 
 if __name__ == '__main__':
     unittest.main()
