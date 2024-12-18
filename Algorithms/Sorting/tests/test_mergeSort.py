4bad21d51fa7a519eec753df9eb53f006a191960
Max Anchutin <100233370+Maxillas@users.noreply.github.com>
2024-12-18 14:53:33 +0500

    Update test_mergeSort.py

--- Algorithms/Sorting/tests/test_mergeSort.py
+++ Algorithms/Sorting/tests/test_mergeSort.py
@@ -4,11 +4,11 @@ class TestMergeSort(unittest.TestCase):
     
     def test_Divide(self):
         test1 = [1,5,7,4,2,3,6]
-        result = Divide(test1)
+        result = MergeSort(test1)
         self.assertEqual(result, [1,2,3,4,5,6,7])
 
         test2 = [1,5,7,4,2,3]
-        result = Divide(test2)
+        result = MergeSort(test2)
         self.assertEqual(result, [1,2,3,4,5,7])
         
 
