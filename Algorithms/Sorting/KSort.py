ebcf39b916a905fcb9099c6ca50073de746a1e50
Max Anchutin <100233370+Maxillas@users.noreply.github.com>
2025-01-09 12:57:37 +0500

    Update KSort.py

--- Algorithms/Sorting/KSort.py
+++ Algorithms/Sorting/KSort.py
@@ -26,66 +26,12 @@ class ksort:
             if(ord(s[2]) == i):
                 d2 = i - 48
                 break
-            
-        print(s, d0, d1, d2)
 
         return int(d0 * 100 + d1 * 10 + d2)
-        #return ord(s[0]) - ord('a')
         
     def add(self, string):
         idx = self.index(string)
         if idx == -1:
             return False
-        # for i in range(len(self.items)):
-        #     if self.items[i] is None or (idx < 0 and i > idx) or (idx >= 0 and i <= idx):
-        #         break
         self.items.insert(idx, string)
         return True     
-
-
-class TestBST(unittest.TestCase):
-    
-    def test_index(self):
-        s1 = "a01"
-        s2 = "b64"
-        s3 = "g99"
-        k = ksort()
-        # self.assertEqual(k.index(s1), 699)
-        # self.assertEqual(k.index(s2), 699)
-        # self.assertEqual(k.index(s3), 699)
-        # self.assertEqual(k.index("132"), -1)
-        # self.assertEqual(k.index("n32"), -1)
-        # self.assertEqual(k.index("ag32"), -1)
-        # self.assertEqual(k.index("a321"), -1)
-
-    def test_add(self):
-        s1 = "a01"
-        s2 = "b64"
-        s3 = "g99"
-        s4 = "a02"
-        s5 = "a06"
-        s6 = "a04"
-        k = ksort()
-        self.assertEqual(k.add(s1), True)
-        #print(k.items)
-        #self.assertEqual(k.items[699], "a01")
-
-        self.assertEqual(k.add(s2), True)
-        #print(k.items)
-        #self.assertEqual(k.items[699], "a01")
-        #self.assertEqual(k.items[698], "b64")
-        
-        self.assertEqual(k.add(s3), True)
-        self.assertEqual(k.add(s4), True)
-        self.assertEqual(k.add(s5), True)
-        self.assertEqual(k.add(s6), True)
-        print(k.items)
-
-    
-        #self.assertEqual(k.items[699], "a01")
-        #self.assertEqual(k.items[698], "b64")
-        #self.assertEqual(k.items[697], "g99")
-
-
-if __name__ == '__main__':
-    unittest.main()
