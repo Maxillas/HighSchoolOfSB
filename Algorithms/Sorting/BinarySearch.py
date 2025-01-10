1f0dd1c97e7fdb0406eee059553d2997995ae042
Max Anchutin <100233370+Maxillas@users.noreply.github.com>
2025-01-10 12:59:01 +0500

    Create BinarySearch.py

---
+++ Algorithms/Sorting/BinarySearch.py
@@ -0,0 +1,34 @@
+class BinarySearch:
+
+    def __init__(self, array):
+        if not array:
+            raise ValueError("Массив не может быть пустым.")
+        self.left = 0
+        self.right = len(array) - 1
+        self.array = array
+        self.complete = 0
+
+    def Step(self, N):
+        if(self.complete != 0):
+            return
+        
+        centerIndex = self.left + (self.right - self.left) // 2
+        center = self.array[centerIndex]
+
+        if(center == N):
+            self.complete = 1
+            return
+        if(N < center):
+            self.right = centerIndex - 1
+        if(N > center):
+            self.left = centerIndex + 1
+
+        if(self.left >= self.right):
+            if(self.array[self.left] == N or self.array[self.right] == N):
+                self.complete = 1
+                return
+            self.complete = -1
+            return
+        
+    def GetResult(self):
+        return self.complete
