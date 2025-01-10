da5eef85a505db5fb7b4c54ea7702466082d968f
Max Anchutin <100233370+Maxillas@users.noreply.github.com>
2025-01-10 13:49:41 +0500

    Update BinarySearch.py

--- Algorithms/Sorting/BinarySearch.py
+++ Algorithms/Sorting/BinarySearch.py
@@ -12,20 +12,20 @@ class BinarySearch:
         if(self.complete != 0):
             return
         
-        centerIndex = self.left + (self.right - self.left) // 2
+        centerIndex = self.Left + (self.Right - self.Left) // 2
         center = self.array[centerIndex]
 
         if(center == N):
             self.complete = 1
             return
         elif(N < center):
-            self.right = centerIndex - 1
+            self.Right = centerIndex - 1
         elif(N > center):
-            self.left = centerIndex + 1
+            self.Left = centerIndex + 1
 
-        if(self.left >= self.right):
-            if 0 <= self.left < len(self.array) and 0 <= self.right < len(self.array):
-                if(self.array[self.left] == N or self.array[self.right] == N):
+        if(self.Left >= self.Right):
+            if 0 <= self.Left < len(self.array) and 0 <= self.Right < len(self.array):
+                if(self.array[self.Left] == N or self.array[self.Right] == N):
                     self.complete = 1
                     return
             self.complete = -1
