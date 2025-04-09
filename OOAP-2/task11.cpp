a9ae4eb3248cbd25095bfe9ebfa6d5820b88299f
Maxillas <maksanchutin@mail.ru>
2025-04-09 17:09:07 +0500

    add: task11

---
+++ OOAP-2/task11.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+
+class General {
+public:
+    virtual void print() = 0;
+};
+
+class Any : virtual public General {
+public:
+    void print() override {
+        std::cout << "copy from Any" << std::endl;
+    }
+    void anySpecific() {
+		std::cout << "From Any" << std::endl;
+    }
+};
+
+class Other : virtual public General {
+public:
+    void print() override {
+        std::cout << "copy from Other" << std::endl;
+    }
+    void otherSpecific() {
+		std::cout << "From Other" << std::endl;
+    }
+};
+
+//Добавили виртуальное наследование, чтобы избежать неоднозначности при вызове метода через указатель на None
+class None : public Any,  public Other {
+	// пустой внутри
+public:
+	void print() override {}
+};
+
+int main () {
+	Any* true_any = new Any();
+	None* none = new None();
+
+	none->anySpecific();// выведет From Any
+	none->otherSpecific(); // выведет From Other
+
+	return 0;
+}
