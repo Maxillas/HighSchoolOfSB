177e88b3fbbc9fd1bdc06fea598f512a3ad00761
Maxillas <maksanchutin@mail.ru>
2026-02-02 17:48:03 +0500

    add lesson9

---
+++ ClearDesign/ClearArchitecture/Lesson09/functional_in_all_dependency_injection.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <sstream>
+#include <list>
+
+// Сейчас в АПИ хранится объект необходимого робота
+// Мы может менять его при инициализации на нужную нам реализацию
+// Тем самым мы передаем только одну функцию (объект)
+struct RobotState {
+    int x = 0;
+    int y = 0;
+    int direction = 0; // 0: вверх, 1: вправо, 2: вниз, 3: влево
+    bool is_cleaning = false;
+    int cleaner_state = 0;
+};
+
+class IRobot {
+public:
+	virtual void transfer_to_cleaner() = 0;
+	virtual ~IRobot();
+	virtual RobotState move(const RobotState& state) = 0;
+	virtual RobotState turn(const RobotState& state) = 0;
+	virtual RobotState set_state(const RobotState& state) = 0;
+	virtual RobotState start(const RobotState& state) = 0;
+	virtual RobotState stop(const RobotState& state) = 0;
+	virtual RobotState make(int transfer, std::string command, const RobotState& state) = 0;
+};
+
+// Функциональная реализация чистильщика, ими	тируе	м
+class PureRobot : public IRobot{
+public:
+	PureRobot() : IRobot() {}
+	void transfer_to_cleaner() override;
+	RobotState move(const RobotState& state) override;
+	RobotState turn(const RobotState& state) override;
+	RobotState set_state(const RobotState& state) override;
+	RobotState start(const RobotState& state) override;
+	RobotState stop(const RobotState& state) override;
+	RobotState make(int transfer, std::string command, const RobotState& state) override;
+};
+
+class Api {
+public:
+	Api() {}
+	~Api() {}
+	void init(IRobot* robot) {
+		m_robot = robot;
+	}
+	void start(IRobot* fooRobot) {
+		state = m_robot->start(state);
+	}
+
+	void stop(IRobot* fooRobot) {
+		state = m_robot->stop(state);
+	}
+
+	void move(IRobot* fooRobot) {
+		state = m_robot->move(state);
+	}
+
+	void turn (IRobot* fooRobot) {
+		state = m_robot->turn(state);
+	}
+
+	void makeCommand(std::string command, int transfer) {
+		state = m_robot->make(transfer, command, state);
+	}
+
+private:
+	RobotState state;
+	IRobot* m_robot;
+};
