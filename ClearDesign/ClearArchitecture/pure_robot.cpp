291120054b04255842e3d87dede11aebaa0ed954
Maxillas <maksanchutin@mail.ru>
2026-02-04 19:33:18 +0500

    change realization robot

--- ClearDesign/ClearArchitecture/pure_robot.cpp
+++ ClearDesign/ClearArchitecture/pure_robot.cpp
@@ -2,27 +2,34 @@
 #include <sstream>
 #include <list>
 
-// Функциональная реализация чистильщика, имитируем
+struct RobotState {
+    int x = 0;
+    int y = 0;
+    int direction = 0; // 0: вверх, 1: вправо, 2: вниз, 3: влево
+    bool is_cleaning = false;
+    int cleaner_state = 0;
+};
+
 class IRobot {
 public:
 	virtual void transfer_to_cleaner() = 0;
 	virtual ~IRobot();
-	virtual void move(int dist) = 0;
-	virtual void turn(int angle) = 0;
-	virtual void set_state(int state) = 0;
-	virtual void start() = 0;
-	virtual void stop() = 0;
-	virtual void make(int transfer, std::string command) = 0;
+	virtual RobotState move(const RobotState& state) = 0;
+	virtual RobotState turn(const RobotState& state) = 0;
+	virtual RobotState set_state(const RobotState& state) = 0;
+	virtual RobotState start(const RobotState& state) = 0;
+	virtual RobotState stop(const RobotState& state) = 0;
+	virtual RobotState make(int transfer, std::string command, const RobotState& state) = 0;
 };
 
 // Функциональная реализация чистильщика, имитируем
 class PureRobot : IRobot{
 public:
 	void transfer_to_cleaner() override;
-	void move(int dist) override;
-	void turn(int angle) override;
-	void set_state(int state) override;
-	void start() override;
-	void stop() override;
-	void make(int transfer, std::string command) override;
+	RobotState move(const RobotState& state) override;
+	RobotState turn(const RobotState& state) override;
+	RobotState set_state(const RobotState& state) override;
+	RobotState start(const RobotState& state) override;
+	RobotState stop(const RobotState& state) override;
+	RobotState make(int transfer, std::string command, const RobotState& state) override;
 };
